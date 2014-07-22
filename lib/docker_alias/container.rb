892ec2da1eb6a27c589dbfd46a983f398edcca92
Hyo Seong Choi <rorlab@gmail.com>
2014-07-22 14:46:40 +0900

    dockera:con 에서 --name 옵션 삭제함

--- lib/docker_alias/container.rb
+++ lib/docker_alias/container.rb
@@ -2,30 +2,22 @@ module DockerAlias
   class Container
     def self.start
       puts "docker run --name #{DockerAlias.run_name} #{DockerAlias.options} -d #{DockerAlias.build_name}"
-    end
-    def self.start_exec
       system "docker run --name #{DockerAlias.run_name} #{DockerAlias.options} -d #{DockerAlias.build_name}"
     end
 
     def self.stop
-      puts "docker stop --name #{DockerAlias.build_name}"
-    end
-    def self.stop_exec
-      system "docker stop --name #{DockerAlias.build_name}"
+      puts "docker stop #{DockerAlias.build_name}"
+      system "docker stop #{DockerAlias.build_name}"
     end
 
     def self.rm
-      puts "docker rm --name #{DockerAlias.build_name}"
-    end
-    def self.rm_exec
-      system "docker rm --name #{DockerAlias.build_name}"
+      puts "docker rm #{DockerAlias.build_name}"
+      system "docker rm #{DockerAlias.build_name}"
     end
 
     def self.bash
       puts "#{DockerAlias.console} /bin/bash"
-    end
-    def self.bash_exec
-      exec "#{DockerAlias.console} /bin/bash"
+      system "#{DockerAlias.console} /bin/bash"
     end
   end
 end
--- lib/docker_alias/db.rb
+++ lib/docker_alias/db.rb
@@ -2,36 +2,26 @@ module DockerAlias
   class Db
     def self.create
       puts "#{DockerAlias.console} bundle exec rake db:create"
-    end
-    def self.create_exec
       system "#{DockerAlias.console} bundle exec rake db:create"
     end
 
     def self.setup
       puts "#{DockerAlias.console} bundle exec rake db:setup"
-    end
-    def self.setup_exec
       system "#{DockerAlias.console} bundle exec rake db:setup"
     end
 
     def self.reset
       puts "#{DockerAlias.console} bundle exec rake db:reset"
-    end
-    def self.reset_exec
       system "#{DockerAlias.console} bundle exec rake db:reset"
     end
 
     def self.migrate
       puts "#{DockerAlias.console} bundle exec rake db:migrate"
-    end
-    def self.migrate_exec
       system "#{DockerAlias.console} bundle exec rake db:migrate"
     end
 
     def self.schema_load
       puts "#{DockerAlias.console} bundle exec rake db:schema:load"
-    end
-    def self.schema_load_exec
       system "#{DockerAlias.console} bundle exec rake db:schema:load"
     end
   end
