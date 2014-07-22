9a355188c701878a02e07aa99970c53b48d45f20
Hyo Seong Choi <rorlab@gmail.com>
2014-07-22 15:20:20 +0900

    오류 수정

--- lib/docker_alias/db.rb
+++ lib/docker_alias/db.rb
@@ -2,31 +2,31 @@ module DockerAlias
   class Db
     def self.create
       puts ">>> #{DockerAlias.console} bundle exec rake db:create"
-      p "=> "
+      puts "=> "
       system "#{DockerAlias.console} bundle exec rake db:create"
     end
 
     def self.setup
       puts ">>> #{DockerAlias.console} bundle exec rake db:setup"
-      p "=> "
+      puts "=> "
       system "#{DockerAlias.console} bundle exec rake db:setup"
     end
 
     def self.reset
       puts ">>> #{DockerAlias.console} bundle exec rake db:reset"
-      p "=> "
+      puts "=> "
       system "#{DockerAlias.console} bundle exec rake db:reset"
     end
 
     def self.migrate
       puts ">>> #{DockerAlias.console} bundle exec rake db:migrate"
-      p "=> "
+      puts "=> "
       system "#{DockerAlias.console} bundle exec rake db:migrate"
     end
 
     def self.schema_load
       puts ">>> #{DockerAlias.console} bundle exec rake db:schema:load"
-      p "=> "
+      puts "=> "
       system "#{DockerAlias.console} bundle exec rake db:schema:load"
     end
   end
